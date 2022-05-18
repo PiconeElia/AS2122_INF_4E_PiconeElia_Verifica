91b0571a3d04fd65c5196674015d8eb57e381053
PC01 <PC01@INFO5-5372>
2022-05-18 09:06:43 +0200

    Aggiungere i file di progetto.

---
+++ AS2122_INF_4E_PiconeElia_Verifica/Form1.cs
@@ -0,0 +1,10 @@
+namespace AS2122_INF_4E_PiconeElia_Verifica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
